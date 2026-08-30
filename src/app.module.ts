70776746ef828af3f1f2219c51fb028a5d59f4a9
devpolas <195308698+devpolas@users.noreply.github.com>
2026-08-31 00:46:22 +0600

    chor: create user and reports module and controller via nest cli

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
+import { UsersModule } from './users/users.module.js';
+import { ReportsModule } from './reports/reports.module.js';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, ReportsModule],
   controllers: [AppController],
   providers: [AppService],
 })

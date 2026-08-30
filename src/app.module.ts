db08c8d883107b12912da65d9500a3dd94d922ca
devpolas <195308698+devpolas@users.noreply.github.com>
2026-08-31 02:27:57 +0600

    nest/cli: update app module as services

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,14 @@ import { AppService } from './app.service.js';
 import { UsersModule } from './users/users.module.js';
 import { ReportsModule } from './reports/reports.module.js';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/user.entity.js';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'better-sqlite3',
       database: 'db.sqlite',
-      entities: [],
+      entities: [User],
       synchronize: true,
     }),
     UsersModule,

af31b05795b213f39350016401fdf0166efc2dad
devpolas <195308698+devpolas@users.noreply.github.com>
2026-09-01 12:32:41 +0600

    chor: setup default report entity

--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ReportsController } from './reports.controller.js';
 import { ReportsService } from './reports.service.js';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Report } from './report.entity.js';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Report])],
   controllers: [ReportsController],
-  providers: [ReportsService]
+  providers: [ReportsService],
 })
 export class ReportsModule {}
